8333d4be1a25db13d2984aa2922116e2f3001318
jgcaap <jgcaap@gmail.com>
2016-05-08 23:17:41 +0200

    Removing what's already included

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -15,11 +15,7 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
-const char CameraParameters::KEY_TRACK_AREAS[] = "track-areas"; \
-const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual"; \
-const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
+const char CameraParameters::KEY_TRACK_AREAS[] = "track-areas";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
-    static const char KEY_TRACK_AREAS[]; \
-    static const char FOCUS_MODE_MANUAL_POSITION[]; \
-    static const char WHITE_BALANCE_MANUAL_CCT[];
+    static const char KEY_TRACK_AREAS[];
