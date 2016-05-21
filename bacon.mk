04d44f9ff9eb9913d8447f7bf8d780703681765d
jgcaap <jgcaap@gmail.com>
2016-05-21 18:11:07 +0200

    Added CameraK1 and plugins for now

--- bacon.mk
+++ bacon.mk
@@ -62,7 +62,23 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8974
+    camera.msm8974 \
+    coloroscam \
+    coloroscam.plug1 \
+    coloroscam.plug2 \
+    coloroscam.plug3 \
+    coloroscam.plug4 \
+    coloroscam.plug5 \
+    coloroscam.plug6 \
+    coloroscam.plug7 \
+    coloroscam.plug8 \
+    coloroscam.plug9 \
+    coloroscam.plug10 \
+    coloroscam.plug11 \
+    coloroscam.plug12
+
+
+
 
 # Charger
 PRODUCT_PACKAGES += \
